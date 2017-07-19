af8b831d81bc3f3638e082ca66d8ae340f9c9e54
haojg <443842006@qq.com>
2017-07-19 15:34:19 +0800

    test spring cloud

---
+++ testspring/eureka/src/main/java/com/hjg/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.hjg;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(EurekaApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+
+		logger.info("started eureka server ..............");
+
+	}
+}
---
+++ testspring/ribbon/src/main/java/com/hjg/HelloController.java
@@ -0,0 +1,11 @@
+package com.hjg;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2017/7/19 0019.
+ */
+
+@RestController
+public class HelloController {
+}
